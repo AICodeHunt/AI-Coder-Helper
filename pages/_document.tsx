fcac023adf9de2c80e7ea6e83d4c342a69870b20
wangguan <wangguan@wangguandeMacBook-Pro.local>
2023-10-02 13:46:25 +0800

    add baidu google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,44 +5,19 @@ export default function Document() {
     <Html lang="en">
       <Head>
 		{/* Add the Google Analytics script tags here */}
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q03Q3VY7RV"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-0KQD9S8RFN"></script>
 		<script
 		  dangerouslySetInnerHTML={{
 			__html: `
 			  window.dataLayer = window.dataLayer || [];
 			  function gtag(){dataLayer.push(arguments);}
 			  gtag('js', new Date());
-			  gtag('config', 'G-Q03Q3VY7RV');
+			  gtag('config', 'G-0KQD9S8RFN');
 			`,
 		  }}
 		/>
 		{/* baidu analytics */}
 		<script src="/baidu-analytics.js" />
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3672455877501055"
-		     crossOrigin="anonymous"></script>
-			 
-		{/* Additional scripts you want to add */}
-		<script async src="https://fundingchoicesmessages.google.com/i/pub-3672455877501055?ers=1" nonce="5dJWzABNlyJqOhVUEimMVQ"></script>
-		<script nonce="5dJWzABNlyJqOhVUEimMVQ">
-		    {`
-		      (function() {
-		        function signalGooglefcPresent() {
-		          if (!window.frames['googlefcPresent']) {
-		            if (document.body) {
-		              const iframe = document.createElement('iframe');
-		              iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
-		              iframe.style.display = 'none';
-		              iframe.name = 'googlefcPresent';
-		              document.body.appendChild(iframe);
-		            } else {
-		              setTimeout(signalGooglefcPresent, 0);
-		            }
-		          }
-		        }
-		        signalGooglefcPresent();
-		      })();
-		    `}
-		</script>
 	  </Head>
       <body className="bg-[#0E1117]">
         <Main />
--- public/baidu-analytics.js
+++ public/baidu-analytics.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ae152cb2b0b6450d60d0bab1e8a8d527";
+  hm.src = "https://hm.baidu.com/hm.js?5ab15b538f286a0f83296fa5d17a7a5f";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
